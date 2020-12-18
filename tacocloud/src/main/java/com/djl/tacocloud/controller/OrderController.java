e834a45f722bf617b5eadcdd7b30cb51215f3724
djlnet <djlnet@17track.cn>
2020-12-18 18:17:38 +0800

    从页面到具体完善n*m的关联保存到h2数据库的功能实现,有待测试

--- tacocloud/src/main/java/com/djl/tacocloud/controller/OrderController.java
+++ tacocloud/src/main/java/com/djl/tacocloud/controller/OrderController.java
@@ -1,13 +1,14 @@
 package com.djl.tacocloud.controller;
 
 import com.djl.tacocloud.entity.Order;
+import com.djl.tacocloud.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
@@ -18,7 +19,15 @@ import javax.validation.Valid;
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
     /**
      * 初始化下单页面
@@ -39,10 +48,13 @@ public class OrderController {
      * @return
      */
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+        orderRepository.save(order);
+        // 订单保存完成之后，我们就不需要在session中持有它了。实际上，如果我们不把它清理掉，那么订单会继续保留在session中，其中包括与之关联的taco，下一次的订单将会从旧订单中保存的taco开始。所以，processOrder()方法请求了一个SessionStatus参数，并调用了它的setComplete()方法来重置session。
+        sessionStatus.setComplete();
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
---
+++ tacocloud/src/main/java/com/djl/tacocloud/repository/JdbcOrderRepository.java
@@ -0,0 +1,63 @@
+package com.djl.tacocloud.repository;
+
+import com.djl.tacocloud.entity.Order;
+import com.djl.tacocloud.entity.Taco;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author djl
+ * @create 2020/12/18 17:55
+ */
+@Repository
+public class JdbcOrderRepository implements OrderRepository {
+
+    private final SimpleJdbcInsert orderInsert;
+    private final SimpleJdbcInsert orderTacoInsert;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public JdbcOrderRepository(JdbcTemplate jdbcTemplate) {
+        // 第一个实例赋值给了orderInserter实例变量，配置为与Taco_Order表协作，并且假定id属性将会由数据库提供或生成
+        this.orderInsert = new SimpleJdbcInsert(jdbcTemplate).withTableName("Taco_Order").usingGeneratedKeyColumns("id");
+        // 第二个实例赋值给了orderTacoInserter实例变量，配置为与Taco_Order_Tacos表协作，但是没有声明该表中ID是如何生成的。
+        this.orderTacoInsert = new SimpleJdbcInsert(jdbcTemplate).withTableName("Taco_Order_Tacos");
+        this.objectMapper = new ObjectMapper();
+    }
+
+    private long saveOrderDetails(Order order) {
+        final Map values = objectMapper.convertValue(order, Map.class);
+        // Map创建完成之后，我们将Map中placedAt条目的值设置为Order对象placedAt属性的值。之所以需要这样做，是因为ObjectMapper会将Date属性转换为long，这会导致与Taco_Order表中的placedAt字段不兼容。
+        values.put("placedAt", order.getPlacedAt());
+        final long orderId = orderInsert.executeAndReturnKey(values).longValue();
+        return orderId;
+    }
+
+    private void saveTacoToOrder(Taco taco, long orderId) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("tacoOrder", orderId);
+        values.put("taco", taco.getId());
+        orderTacoInsert.execute(values);
+    }
+
+    @Override
+    public Order save(Order order) {
+        order.setPlacedAt(new Date());
+        final long orderId = saveOrderDetails(order);
+        order.setId(orderId);
+        final List<Taco> tacos = order.getTacos();
+        for (Taco taco : tacos) {
+            saveTacoToOrder(taco, orderId);
+        }
+        return order;
+    }
+}
