d1f0fab4d6ea9b53cdacc0d1a34923a5458ef8f6
djlnet <djlnet@17track.cn>
2020-12-18 10:28:28 +0800

    增加初始化spring5的实战项目工程

---
+++ tacocloud/src/main/java/com/djl/tacocloud/TacocloudApplication.java
@@ -0,0 +1,13 @@
+package com.djl.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacocloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacocloudApplication.class, args);
+	}
+
+}
---
+++ tacocloud/src/test/java/com/djl/tacocloud/TacocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djl.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
