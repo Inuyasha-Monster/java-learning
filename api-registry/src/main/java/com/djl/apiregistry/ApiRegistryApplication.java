abf2114fb90ce77f6be672d15f80bbfd9d0c7f6e
djlnet <djlnet@17track.cn>
2020-12-21 18:19:11 +0800

    增加服务注册和发现的api工程

---
+++ api-registry/src/main/java/com/djl/apiregistry/ApiRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.djl.apiregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRegistryApplication.class, args);
+	}
+
+}
---
+++ api-registry/src/test/java/com/djl/apiregistry/ApiRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djl.apiregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
