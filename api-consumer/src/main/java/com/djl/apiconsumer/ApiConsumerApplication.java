e7b7db932139d95988a24c2f51b941c4b9f5e6e5
djlnet <djlnet@17track.cn>
2020-12-22 11:13:22 +0800

    服务注册和服务发现的总结: •借助自动配置和@EnableEurekaServer注解，Spring Cloud Netflix能够让我们很容易地创建Netflix Eureka服务注册中心。 •微服务可以使用名字将它们自身注册到Eureka中，这样可以被其他服务发现。 •在客户端，作为客户端负载均衡器，Ribbon能够根据名称查找服务并选择实例。 •客户端代码可以使用RestTemplate，利用Ribbon进行负载均衡；也可以将REST客户端定义为接口，由Feign在运行期自动实现。 •不管采用哪种方案，客户端代码都不需要硬编码它们所消费的服务的地址。

--- api-consumer/src/main/java/com/djl/apiconsumer/ApiConsumerApplication.java
+++ api-consumer/src/main/java/com/djl/apiconsumer/ApiConsumerApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class ApiConsumerApplication {
 
     public static void main(String[] args) {
@@ -20,5 +23,11 @@ public class ApiConsumerApplication {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
--- api-consumer/src/main/java/com/djl/apiconsumer/controller/TestController.java
+++ api-consumer/src/main/java/com/djl/apiconsumer/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.djl.apiconsumer.controller;
 
+import com.djl.apiconsumer.feign.ApiClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 /**
  * @author djl
@@ -17,13 +20,34 @@ public class TestController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    WebClient.Builder webClientBuilder;
+
+    @Autowired
+    ApiClientService apiClientService;
+
     /**
      * 从eureka中获取远程服务地址然后采取默认的轮询的方式调用远程服务
+     *
      * @return
      */
     @GetMapping("/getRemote")
     public Iterable<String> getRemote() {
         final Iterable<String> result = restTemplate.getForObject("http://api-service/api/getList", java.lang.Iterable.class);
         return result;
+    }
+
+    @GetMapping("/getRemote2")
+    public Mono<Iterable<String>> getRemote2() {
+        final Mono<Iterable> result = webClientBuilder.build().get().uri("http://api-service/api/getList")
+                .retrieve().bodyToMono(Iterable.class);
+        final Mono<Iterable<String>> map = result.map(x -> ((Iterable<String>) x));
+        return map;
+    }
+
+    @GetMapping("/getRemote3")
+    public Iterable<String> getRemote3() {
+        final Iterable<String> list = apiClientService.getList();
+        return list;
     }
 }
