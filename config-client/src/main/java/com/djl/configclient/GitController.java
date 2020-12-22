c0962532b60445de4186aa492d3e1c9affd0bc4f
djlnet <djlnet@17track.cn>
2020-12-22 15:17:11 +0800

    完善config server/client实践,其中server采取github作为远程存储仓库,client端结合spring-bus实现广播刷新配置文件

---
+++ config-client/src/main/java/com/djl/configclient/GitController.java
@@ -0,0 +1,32 @@
+package com.djl.configclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author djl
+ * @create 2020/12/22 14:59
+ */
+@RestController
+@RequestMapping("/api")
+@RefreshScope
+public class GitController {
+    @Autowired
+    private GitConfig gitConfig;
+
+    @Autowired
+    private GitAutoRefreshConfig gitAutoRefreshConfig;
+
+    @GetMapping(value = "/show")
+    public Object show() {
+        return gitConfig;
+    }
+
+    @GetMapping(value = "/autoShow")
+    public Object autoShow() {
+        return gitAutoRefreshConfig;
+    }
+}
