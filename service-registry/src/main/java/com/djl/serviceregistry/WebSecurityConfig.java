44ce45f63c961c5e033d13b2596ca4e95d3855bb
djlnet <djlnet@17track.cn>
2020-12-22 16:31:30 +0800

    针对问题:https://github.com/spring-cloud/spring-cloud-netflix/issues/2754 , spring security csrf验证导致erueka-client注册失败的问题采用 http.csrf().ignoringAntMatchers("/eureka/**"); 解决

---
+++ service-registry/src/main/java/com/djl/serviceregistry/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.djl.serviceregistry;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author djl
+ * @create 2020/12/22 16:27
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
