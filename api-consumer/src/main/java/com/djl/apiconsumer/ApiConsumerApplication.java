6e112990b52a19b125c8de8efc1f0123f1ebc628
djlnet <djlnet@17track.cn>
2020-12-29 17:53:44 +0800

    测试负载均衡策略

--- api-consumer/src/main/java/com/djl/apiconsumer/ApiConsumerApplication.java
+++ api-consumer/src/main/java/com/djl/apiconsumer/ApiConsumerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
